a188de6407c20eef5d7d205d8589dfda384a8150
Tapan Thaker <tapan.d.thaker@gmail.com>
2015-11-08 18:47:31 +0530

    Handling scenario where request parsing fails.

--- FBSimulatorCLI/WebServer.swift
+++ FBSimulatorCLI/WebServer.swift
@@ -33,23 +33,32 @@ class WebServer : NSObject {
     private func handleRequest(request : GCDWebServerRequest, handler : RequestHandler, completionBlock : GCDWebServerCompletionBlock) {
         
         let dataRequest = request as! GCDWebServerDataRequest
-        let map = NSJSONSerialization.JSONObjectWithData(dataRequest.data, options: NSJSONReadingOptions.allZeros, error: nil) as! Map
-        NSLog("Request: %@", map)
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            
-            let response = handler.handle(map)
-            var dataResponse  : GCDWebServerDataResponse;
-            if response.isLeft() {
-                dataResponse = GCDWebServerDataResponse(data: NSJSONSerialization.dataWithJSONObject(response.left!, options: NSJSONWritingOptions.allZeros, error: nil) , contentType: "application/json")
-                dataResponse.statusCode = 200;
-            } else {
-                dataResponse = GCDWebServerDataResponse(data: NSJSONSerialization.dataWithJSONObject(response.right!.userInfo!, options: NSJSONWritingOptions.allZeros, error: nil) , contentType: "application/json")
-                
-            }
-            NSLog("Response: %@", dataResponse)
-            completionBlock(dataResponse)
-        })
+        var serializationError : NSError?
+        let map = NSJSONSerialization.JSONObjectWithData(dataRequest.data, options: NSJSONReadingOptions.allZeros, error: &serializationError) as? Map
         
+        if let error = serializationError {
+            completionBlock(self.dataResponseForError(error))
+        } else {
+            if let requestMap = map {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    let response = handler.handle(requestMap)
+                    var dataResponse  : GCDWebServerDataResponse;
+                    if response.isLeft() {
+                        dataResponse = GCDWebServerDataResponse(data: NSJSONSerialization.dataWithJSONObject(response.left!, options: NSJSONWritingOptions.allZeros, error: nil) , contentType: "application/json")
+                        dataResponse.statusCode = 200;
+                    } else {
+                        dataResponse = self.dataResponseForError(response.right!)
+                    }
+                    completionBlock(dataResponse)
+                })
+            }
+        }
+    }
+    
+    private func dataResponseForError(error :NSError) -> GCDWebServerDataResponse {
+        let dataResponse = GCDWebServerDataResponse(data: NSJSONSerialization.dataWithJSONObject(error.userInfo!, options: NSJSONWritingOptions.allZeros, error: nil) , contentType: "application/json")
+        dataResponse.statusCode = 500
+        return dataResponse
     }
     
     func startServer()  {
